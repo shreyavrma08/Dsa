e3ef1a430798c7e176f5b8e2f622b76cb8282463
shreyavrma08 <shreyavsve@gmail.com>
2026-08-01 23:42:09 +0530

    Update doublyLL.py

--- doublyLL.py
+++ doublyLL.py
@@ -1,42 +1,393 @@
+# #Insertion at the End of DoublyLL
+# class Node:
+#     def __init__(self,value=None):
+#         self.data=value
+#         self.next=None
+#         self.prev=None
+# class DoublyLL:
+#     def __init__(self):
+#         self.head=None
+#     def insertAtEnd(self,value):
+#         temp=Node(value)
+#         if(self.head==None):
+#             self.head=temp
+#             return
+#         t=self.head
+#         while (t.next != None):
+#             t=t.next
+#         t.next=temp
+#         temp.prev=t
+
+
+#     def printDLL(self):
+#         t1=self.head
+#         while(t1.next != None):
+#             print(t1.data,end=" <-->")
+#             t1=t1.next
+#         print(t1.data)
+
+# obj=DoublyLL()
+# obj.insertAtEnd(10)
+# obj.insertAtEnd(20)
+# obj.insertAtEnd(30)
+# obj.insertAtEnd(40)
+# # obj.insertAtBeg(5)
+# obj.printDLL()
+
+
+# #Insertion at the Beg of DoublyLL
+# class Node:
+#     def __init__(self,value=None):
+#         self.data=value
+#         self.next=None
+#         self.prev=None
+# class DoublyLL:
+#     def __init__(self):
+#         self.head=None
+#     def insertAtEnd(self,value):
+#         temp=Node(value)
+#         if(self.head==None):
+#             self.head=temp
+#             return
+#         t=self.head
+#         while (t.next != None):
+#             t=t.next
+#         t.next=temp
+#         temp.prev=t
+#     def insertAtBeg(self,value):
+#         temp=Node(value)
+#         if(self.head==None):
+#             self.head=temp
+#             return
+#         temp.next=self.head
+#         self.head.prev=temp
+#         self.head=temp
+
+#     def printDLL(self):
+#         t1=self.head
+#         while(t1.next != None):
+#             print(t1.data,end=" <-->")
+#             t1=t1.next
+#         print(t1.data)
+
+# obj=DoublyLL()
+# obj.insertAtEnd(10)
+# obj.insertAtEnd(20)
+# obj.insertAtEnd(30)
+# obj.insertAtEnd(40)
+# obj.insertAtBeg(5)
+# obj.printDLL()
+
+
+# #Insert at Mid
+# class Node:
+#     def __init__(self,value=None):
+#         self.data=value
+#         self.next=None
+#         self.prev=None
+# class DoublyLL:
+#     def __init__(self):
+#         self.head=None
+#     def insertAtEnd(self,value):
+#         temp=Node(value)
+#         if(self.head==None):
+#             self.head=temp
+#             return
+#         t=self.head
+#         while (t.next != None):
+#             t=t.next
+#         t.next=temp
+#         temp.prev=t
+#     def insertAtBeg(self,value):
+#         temp=Node(value)
+#         if(self.head==None):
+#             self.head=temp
+#             return
+#         temp.next=self.head
+#         self.head.prev=temp
+#         self.head=temp
+#     def insertAtMid(self,value,x):
+#         t=self.head
+#         while(t.next!=None):
+#             if(t.data==x):
+#                 break
+#             else:
+#                 t=t.next
+#         temp=Node(value)
+#         temp.next=t.next
+#         t.next.prev=temp
+#         t.next=temp
+#         temp.prev=t
+
+#     def printDLL(self):
+#         t1=self.head
+#         while(t1.next != None):
+#             print(t1.data,end=" <-->")
+#             t1=t1.next
+#         print(t1.data)
+
+# obj=DoublyLL()
+# obj.insertAtEnd(10)
+# obj.insertAtEnd(20)
+# obj.insertAtEnd(30)
+# obj.insertAtEnd(40)
+# obj.insertAtBeg(5)
+# obj.insertAtMid(50,20)
+# obj.printDLL()
+
+
+# #DEletion in doubly linked list
+
+# class Node:
+#     def __init__(self,value=None):
+#         self.data=value
+#         self.next=None
+#         self.prev=None
+# class DoublyLL:
+#     def __init__(self):
+#         self.head=None
+#     def insertAtEnd(self,value):
+#         temp=Node(value)
+#         if(self.head==None):
+#             self.head=temp
+#             return
+#         t=self.head
+#         while (t.next != None):
+#             t=t.next
+#         t.next=temp
+#         temp.prev=t
+#     def insertAtBeg(self,value):
+#         temp=Node(value)
+#         if(self.head==None):
+#             self.head=temp
+#             return
+#         temp.next=self.head
+#         self.head.prev=temp
+#         self.head=temp
+#     def insertAtMid(self,value,x):
+#         t=self.head
+#         while(t.next!=None):
+#             if(t.data==x):
+#                 break
+#             else:
+#                 t=t.next
+#         temp=Node(value)
+#         temp.next=t.next
+#         t.next.prev=temp
+#         t.next=temp
+#         temp.prev=t
+
+#     def printDLL(self):
+#         t1=self.head
+#         while(t1.next != None):
+#             print(t1.data,end=" <-->")
+#             t1=t1.next
+#         print(t1.data)
+
+#     def deletion(self,value):
+#             if (self.head == None):
+#                 print("LL is empty")
+#                 return
+#             t=self.head
+#             if(t.data == value):
+#                 self.head = t.next
+#                 self.head.prevt=None
+#                 return
+#             while(t.next != None):
+#                 if(t.data == value):
+#                     t.prev.next = t.next
+#                     t.next.prev = t.next
+#                     return
+#                 else:
+#                     t=t.next
+#             if (t.data == value):
+#                     t.prev.next = None
+
+
+
+# obj=DoublyLL()
+# obj.insertAtEnd(10)
+# obj.insertAtEnd(20)
+# obj.insertAtEnd(30)
+# obj.insertAtEnd(40)
+# obj.insertAtBeg(5)
+# obj.insertAtMid(50,20)
+# obj.deletion(5)
+# obj.deletion(50)
+# obj.deletion(40)
+# obj.printDLL()
+
+# #Reverse of a doublyLL
+
+
+#                           #Reverse of linkedlist 
+# #TC=O(2n)=O(2n)         SC=O(n)
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.next = None
+#         self.prev=None
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+#     def reverse(self):
+#         temp=self.head
+#         stack=[]
+#         while temp is not None:
+#             stack.append(temp.val)
+#             temp=temp.next
+#         temp=self.head
+#         while temp is not None:
+#                 temp.val=stack.pop()
+                
+#                 temp=temp.next
+#     def display(self):
+#         temp = self.head
+#         while temp is not None:
+#             print(temp.val, end="->")
+#             temp = temp.next
+             
+#         print("None")
+    
+# #creating linked list
+# l = LinkedList()
+
+# l.head = Node(5)
+# l.head.next = Node(8)
+# l.head.next.next = Node(7)
+# l.head.next.next.next = Node(3)
+# l.head.next.next.next.next = Node(2)
+# print("Original Linked List:")
+# l.display()
+
+# l.reverse()
+
+# print("Reversed Linked List:")
+# l.display()
+
+
+#  #                           Optimal Solution Of reverse
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.next = None
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+#     def reverse(self):
+#         curr=self.head
+#         prev=None
+#         while curr is not None:
+#             front=curr.next
+#             curr.next=prev
+#             curr.prev=front
+#             prev=curr
+#             curr=front
+#         self.head=prev
+#     def display(self):
+#         temp = self.head
+
+#         while temp is not None:
+#             print(temp.val, end=" -> ")
+#             temp = temp.next
+
+#         print("None")
+# #creating linked list
+# l = LinkedList()
+
+# l.head = Node(5)
+# l.head.next = Node(8)
+# l.head.next.next = Node(7)
+# l.head.next.next.next = Node(3)
+# l.head.next.next.next.next = Node(2)
+# print("Original Linked List:")
+# l.display()
+
+# l.reverse()
+
+# print("Reversed Linked List:")
+# l.display()
+
+
+#Deleting All Ocurence of a key in DoublyLL
 class Node:
-    def __init__(self,value=None):
-        self.data=value
-        self.next=None
-        self.prev=None
-class DoublyLL:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class LinkedList:
+
     def __init__(self):
-        self.head=None
-    def insertAtEnd(self,value):
-        temp=Node(value)
-        if(self.head==None):
-            self.head=temp
+        self.head = None
+
+    def insert_at_end(self, val):
+        new_node = Node(val)
+
+        if self.head is None:
+            self.head = new_node
             return
-        t=self.head
-        while (t.next != None):
-            t=t.next
-        t.next=temp
-        temp.prev=t
-    def insertAtBeg(self,value):
-        temp=Node(value)
-        if(self.head==None):
-            self.head=temp
-            return
-        temp.next=self.head
-        self.head.prev=temp
-        self.head=temp
 
-    def printDLL(self):
-        t1=self.head
-        while(t1.next != None):
-            print(t1.data,end=" <-->")
-            t1=t1.next
-        print(t1.data)
+        temp = self.head
 
-obj=DoublyLL()
-obj.insertAtEnd(10)
-obj.insertAtEnd(20)
-obj.insertAtEnd(30)
-obj.insertAtEnd(40)
-obj.insertAtBeg(5)
-obj.printDLL()
+        while temp.next is not None:
+            temp = temp.next
 
+        temp.next = new_node
+        new_node.prev = temp
+
+    def delete_occurrence(self, k):
+        temp = self.head
+
+        while temp is not None:
+
+            if temp.val == k:
+
+                # If deleting head
+                if temp == self.head:
+                    self.head = temp.next
+
+                    if self.head is not None:
+                        self.head.prev = None
+
+                else:
+                    # Connect previous node to next node
+                    temp.prev.next = temp.next
+
+                    # Connect next node to previous node
+                    if temp.next is not None:
+                        temp.next.prev = temp.prev
+
+            temp = temp.next
+
+    def display(self):
+        temp = self.head
+
+        while temp is not None:
+            print(temp.val, end=" <-> ")
+            temp = temp.next
+
+        print("None")
+
+
+# Creating linked list
+l = LinkedList()
+
+l.insert_at_end(5)
+l.insert_at_end(2)
+l.insert_at_end(3)
+l.insert_at_end(2)
+l.insert_at_end(10)
+
+print("Original:")
+l.display()
+
+l.delete_occurrence(2)
+
+print("After deleting 2:")
+l.display()

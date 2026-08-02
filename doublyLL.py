837e7103fe2fd6e09d00c054cea7920c03d1209c
shreyavrma08 <shreyavsve@gmail.com>
2026-08-02 09:40:58 +0530

    Update doublyLL.py

--- doublyLL.py
+++ doublyLL.py
@@ -313,7 +313,152 @@
 # l.display()
 
 
-#Deleting All Ocurence of a key in DoublyLL
+# #Deleting All Ocurence of a key in DoublyLL
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.next = None
+#         self.prev = None
+
+
+# class LinkedList:
+
+#     def __init__(self):
+#         self.head = None
+
+#     def insert_at_end(self, val):
+#         new_node = Node(val)
+
+#         if self.head is None:
+#             self.head = new_node
+#             return
+
+#         temp = self.head
+
+#         while temp.next is not None:
+#             temp = temp.next
+
+#         temp.next = new_node
+#         new_node.prev = temp
+
+#     def delete_occurrence(self, k):
+#         temp = self.head
+
+#         while temp is not None:
+
+#             if temp.val == k:
+
+#                 # If deleting head
+#                 if temp == self.head:
+#                     self.head = temp.next
+
+#                     if self.head is not None:
+#                         self.head.prev = None
+
+#                 else:
+#                     # Connect previous node to next node
+#                     temp.prev.next = temp.next
+
+#                     # Connect next node to previous node
+#                     if temp.next is not None:
+#                         temp.next.prev = temp.prev
+
+#             temp = temp.next
+
+#     def display(self):
+#         temp = self.head
+
+#         while temp is not None:
+#             print(temp.val, end=" <-> ")
+#             temp = temp.next
+
+#         print("None")
+
+
+# # Creating linked list
+# l = LinkedList()
+
+# l.insert_at_end(5)
+# l.insert_at_end(2)
+# l.insert_at_end(3)
+# l.insert_at_end(2)
+# l.insert_at_end(10)
+
+# print("Original:")
+# l.display()
+
+# l.delete_occurrence(2)
+
+# print("After deleting 2:")
+# l.display()
+
+#Find pairs with given sum in DoublyLL
+#Brute :  TC=O(n^2)   SC=O(1)
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def find_pairs(self, target):
+        temp1=self.head
+        result=[]
+        while temp1 is not None:
+           temp2=temp1.next
+           while temp2 is not None:
+                if temp1.val + temp2.val == target:
+                    result.append([temp1.val,temp2.val])
+                temp2=temp2.next
+           temp1=temp1.next
+        print(result)
+#Creating linked list
+l = LinkedList()
+l.head = Node(1)
+l.head.next = Node(2)
+l.head.next.next = Node(3)
+l.head.next.next.next = Node(4)
+l.head.next.next.next.next = Node(5)
+l.head.next.next.next.next.next = Node(6)
+l.head.next.next.next.next.next.next = Node(8)
+l.head.next.next.next.next.next.next.next = Node(9)
+l.find_pairs(7)
+
+#Better :  TC=O(n^2)   SC=O(1)
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def find_pairs(self, target):
+        my_set=set()
+        temp=self.head
+        result=[]
+        while temp is not None:
+            remaining=target-temp.val
+  
+            if remaining in my_set:
+                    result.append([remaining,temp.val])
+            my_set.add(temp.val)
+            temp=temp.next   
+        print(result)
+#Creating linked list
+l = LinkedList()
+l.head = Node(1)
+l.head.next = Node(2)
+l.head.next.next = Node(4)
+l.head.next.next.next = Node(5)
+l.head.next.next.next.next = Node(6)
+l.head.next.next.next.next.next = Node(8)
+l.head.next.next.next.next.next.next = Node(9)
+
+l.find_pairs(7)
+
+#Optimal :  TC=O(n^2)   SC=O(1)
 class Node:
     def __init__(self, val):
         self.val = val
@@ -322,72 +467,71 @@ class Node:
 
 
 class LinkedList:
-
     def __init__(self):
         self.head = None
 
-    def insert_at_end(self, val):
-        new_node = Node(val)
+    def find_pairs(self, target):
+        left = self.head
+        right = self.head
+        result = []
 
-        if self.head is None:
-            self.head = new_node
-            return
+        while right.next is not None:
+            right = right.next
 
-        temp = self.head
+        while left is not None and right is not None and left.val < right.val:
+            total = left.val + right.val
 
-        while temp.next is not None:
-            temp = temp.next
+            if total == target:
+                result.append([left.val, right.val])
+                left = left.next
+                right = right.prev
 
-        temp.next = new_node
-        new_node.prev = temp
+            elif total > target:
+                right = right.prev
 
-    def delete_occurrence(self, k):
-        temp = self.head
+            else:
+                left = left.next
 
-        while temp is not None:
-
-            if temp.val == k:
-
-                # If deleting head
-                if temp == self.head:
-                    self.head = temp.next
-
-                    if self.head is not None:
-                        self.head.prev = None
-
-                else:
-                    # Connect previous node to next node
-                    temp.prev.next = temp.next
-
-                    # Connect next node to previous node
-                    if temp.next is not None:
-                        temp.next.prev = temp.prev
-
-            temp = temp.next
-
-    def display(self):
-        temp = self.head
-
-        while temp is not None:
-            print(temp.val, end=" <-> ")
-            temp = temp.next
-
-        print("None")
+        print(result)
 
 
 # Creating linked list
 l = LinkedList()
 
-l.insert_at_end(5)
-l.insert_at_end(2)
-l.insert_at_end(3)
-l.insert_at_end(2)
-l.insert_at_end(10)
+l.head = Node(1)
 
-print("Original:")
-l.display()
+l.head.next = Node(2)
+l.head.next.prev = l.head
 
-l.delete_occurrence(2)
+l.head.next.next = Node(3)
+l.head.next.next.prev = l.head.next
 
-print("After deleting 2:")
-l.display()
+l.head.next.next.next = Node(4)
+l.head.next.next.next.prev = l.head.next.next
+
+l.head.next.next.next.next = Node(5)
+l.head.next.next.next.next.prev = l.head.next.next.next
+
+l.head.next.next.next.next.next = Node(6)
+l.head.next.next.next.next.next.prev = l.head.next.next.next.next
+
+l.head.next.next.next.next.next.next = Node(8)
+l.head.next.next.next.next.next.next.prev = l.head.next.next.next.next.next
+
+l.head.next.next.next.next.next.next.next = Node(9)
+l.head.next.next.next.next.next.next.next.prev = l.head.next.next.next.next.next.next
+
+l.find_pairs(7)
+
+# #Creating linked list
+# l = LinkedList()
+# l.head = Node(1)
+# l.head.next = Node(2)
+# l.head.next.next = Node(3)
+# l.head.next.next.next = Node(4)
+# l.head.next.next.next.next = Node(5)
+# l.head.next.next.next.next.next = Node(6)
+# l.head.next.next.next.next.next.next = Node(8)
+# l.head.next.next.next.next.next.next.next = Node(9)
+# l.find_pairs(7)
+

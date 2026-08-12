065bc7bda1465a51620050b103acf7cc8084f420
shreyavrma08 <shreyavsve@gmail.com>
2026-08-12 09:06:34 +0530

    Update recursion1.py

--- Recursion/recursion1.py
+++ Recursion/recursion1.py
@@ -45,7 +45,43 @@ s="anbcddcbna"
 print(isPalindrome(s,0,len(s)-1))
 
 
+# def isPalindrome(self, s):
+
+#         left = 0
+#         right = len(s) - 1
+
+#         while left < right:
+
+#             while left < right and not (s[left].isalpha() or s[left].isdigit()):
+#                 left += 1
+
+#             while left < right and not (s[right].isalpha() or s[right].isdigit()):
+#                 right -= 1
+
+#             if s[left].lower() != s[right].lower():
+#                 return False
+
+#             left += 1
+#             right -= 1
+
+#         return True
+
+# s="anBcddCbna"
+# print(isPalindrome(s,0,len(s)))
+
+#Leetcode 125 Valid palinedrome.
+def isPalindrome(s, left, right):
+
+    if left >= right:
+        return True
+
+    if s[left].lower() != s[right].lower():
+        return False
+
+    return isPalindrome(s, left + 1, right - 1)
 
 
-        
+s = "mAdam"
+
+print(isPalindrome(s, 0, len(s) - 1))
       

9c30067a2f6090dbd83a97aa22518c64c3711e57
shreyavrma08 <shreyavsve@gmail.com>
2026-08-12 11:26:36 +0530

    Create Selection.py

---
+++ Sorting/Selection.py
@@ -0,0 +1,11 @@
+def selection_sort(nums):
+    n=len(nums)
+    for i in range(0,n):
+        min_ind=i
+        for j in range(i+1,n):
+            if nums[j]<nums[min_ind]:
+                min_ind=j
+        nums[i],nums[min_ind]=nums[min_ind],nums[i]
+    return nums
+nums=[5,7,8,4,1,6,9,2]
+print(selection_sort(nums))

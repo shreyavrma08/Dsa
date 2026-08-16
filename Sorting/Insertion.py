c219d420c0ed5bfe9067d9bbf3f2b38fb81594ed
shreyavrma08 <shreyavsve@gmail.com>
2026-08-16 15:16:58 +0530

    Update Insertion.py

---
+++ Sorting/Insertion.py
@@ -0,0 +1,14 @@
+def insertion(nums):
+    n=len(nums)
+    for i in range(1,n):
+        key=nums[i]
+        j=i-1
+        while j>=0 and nums[j]>key:
+            nums[j+1]=nums[j]
+            j-=1
+        nums[j+1]=key
+    return nums
+nums=[2,5,6,3,8,9,10,7,1]
+print(insertion(nums))
+
+

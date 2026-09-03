869a4e04322352b7cfbcdb32ef7a221219725f23
shreyavrma08 <shreyavsve@gmail.com>
2026-09-03 07:34:51 +0530

    Update p.py

--- python.py/p.py
+++ python.py/p.py
@@ -1,7 +1,44 @@
-def sum(a,b):
-    for i in range(a,b+1):
-        sum+=1
-    print(sum)
-a=1
-b=9
-print(sum)
+# a="shreya"
+# b="verma"
+# c="shreyaverma"
+# print(a+b)
+# print(len(a+b))
+# print(a[0])
+# print(a[-1])
+# print(c[0::2])
+# print(c[-1::-2])
+
+# z="my name is Shreya Verma.IM persuing btech from babu banarasi das university"
+# print(z.endswith("y"))
+# print(z.capitalize())
+# print(z.find("a"))
+# print(z.replace("name",'verma'))
+# print(z.replace("a",'o'))
+
+# a=input('num=')
+# b=a
+# a=a[::-1]
+# if a==b:
+#     print("Palindrome")
+# else:
+#     print("Not palindrome")
+
+
+# d=input("Enter")
+# v=0
+# for i in d:
+#     if (i=='e' or i=="a" or i =='i'   or i=='o'  or i=='u'):
+#         v+=1
+# print(v)
+
+a="hello"
+# for i in range(len(a)):
+#     print(a[i])
+# for j in range(len(a)-1,-1,-1):
+#     print(a[j])
+b=0
+while(b<len(a)):
+    print
+
+
+

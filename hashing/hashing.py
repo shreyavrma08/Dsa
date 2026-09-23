20ac47cd0f6a8395484a91c0c5fc6ef451bb71e1
shreyavrma08 <shreyavsve@gmail.com>
2026-09-23 15:38:22 +0530

    Update hashing.py

--- hashing/hashing.py
+++ hashing/hashing.py
@@ -9,3 +9,16 @@ number=1
 arr=[1,2,1,1,3,4]
 result=f(number,arr)
 print(result)
+
+
+#Leetcode 1: two sum TC=O(n)   SC=O(n)
+num=[2,6,5,8,11]
+target=14
+hash_table={}
+for i in range(len(num)):
+    complement=target - num[i]
+    if complement in hash_table:
+        print(hash_table[complement],i)
+    hash_table[num[i]]=i
+print()
+        
